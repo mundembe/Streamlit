9302acc9a9e16e9a23475d6076901346991f54ff
mundembe <61733352+mundembe@users.noreply.github.com>
2022-07-20 15:21:35 +0200

    updating location of stylesheet (line 18)

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("style.css")
 
 #-----------Load Assets-----------
 lottie_animation = load_lottie_url("https://assets8.lottiefiles.com/packages/lf20_we4yddwi.json")
